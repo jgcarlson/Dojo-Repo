b0cc20fdca0e52fa62e32919b4cfdeb359997365
jonathan <jgcarlson@outlook.com>
2017-05-30 14:56:14 -0700

    completed jquery functions project

--- Web Fundamentals/Javascript/jQuery Functions/script.js
+++ Web Fundamentals/Javascript/jQuery Functions/script.js
@@ -1,97 +1,89 @@
 $(document).ready(function() {
 
-      var newDiv = $('<div class="container" style="text-align:center"><h1>This is all new stuff</h1></div>')
+  var newDiv = $('<div class="container" style="text-align:center"><h1>This is all new stuff</h1></div>')
 
-      /* --- Click --- */
-      $('#blue').click(function() {
-        $(this).css('background', 'blue');
-      });
+  /* --- Click --- */
+  $('#blue').click(function() {
+    $(this).css('background', 'blue');
+  });
 
-      /* --- Hide/Show --- */
-      $('#hide').click(function() {
-        $('#showhidetext').hide();
-      });
+  /* --- Hide/Show --- */
+  $('#hide').click(function() {
+    $('#showhidetext').hide();
+  });
 
-      $('#show').click(function() {
-        $('#showhidetext').show();
-      });
+  $('#show').click(function() {
+    $('#showhidetext').show();
+  });
 
-      /* --- Slide Up/Down --- */
-      $('#up').click(function() {
-        $('#updown').slideUp('slow');
-      });
+  /* --- Slide Up/Down --- */
+  $('#up').click(function() {
+    $('#updown').slideUp('slow');
+  });
 
-      $('#down').click(function() {
-        $('#updown').slideDown('slow');
-      });
+  $('#down').click(function() {
+    $('#updown').slideDown('slow');
+  });
 
-      /* --- Slide Toggle --- */
-      $('#slidetoggle').click(function() {
-        $('#updowntoggle').slideToggle('slow');
-      });
+  /* --- Slide Toggle --- */
+  $('#slidetoggle').click(function() {
+    $('#updowntoggle').slideToggle('slow');
+  });
 
-      /* --- Fade Out/In --- */
-      $('#in').click(function() {
-        $('#fade').fadeIn('slow');
-      });
+  /* --- Fade Out/In --- */
+  $('#in').click(function() {
+    $('#fade').fadeIn('slow');
+  });
 
-      $('#out').click(function() {
-        $('#fade').fadeOut('slow');
-      });
+  $('#out').click(function() {
+    $('#fade').fadeOut('slow');
+  });
 
-      /* --- Add Class --- */
-      $('#addclass').click(function() {
-        $('#target').addClass("classme");
-      });
+  /* --- Add Class --- */
+  $('#addclass').click(function() {
+    $('#target').addClass("classme");
+  });
 
-      /* --- Before/After --- */
-      $('#before').click(function() {
-        $('#beforeafter').before(newDiv);
-      });
+  /* --- Before/After --- */
+  $('#before').click(function() {
+    $('#beforeafter').before(newDiv);
+  });
 
-      $('#after').click(function() {
-        $('#beforeafter').after(newDiv);
-      });
+  $('#after').click(function() {
+    $('#beforeafter').after(newDiv);
+  });
 
-      /* --- Append --- */
-      $('#append').click(function() {
-        $('#thiscontainer').append('<p>You just appended a "p" element.</p>');
-      });
+  /* --- Append --- */
+  $('#append').click(function() {
+    $('#thiscontainer').append('<p>You just appended a "p" element.</p>');
+  });
 
-      /* --- HTML --- */
-      $('#html').click(function() {
-        $('#replace').html('<p>This is new HTML.</p>');
-      });
+  /* --- HTML --- */
+  $('#html').click(function() {
+    $('#replace').html('<p>This is new HTML.</p>');
+  });
 
-      /* --- Attributes --- */
-      var type = $('button').attr('id');
+  /* --- Attributes --- */
+  var type = $('button').attr('id');
 
-      $('#attributes').click(function() {
-        $('#attr').html(type);
-      });
+  $('#attributes').click(function() {
+    $('#attr').html(type);
+  });
 
-      /* --- Val --- */
-      var val = $('input').val();
-      $('#val').click(function() {
-        $('#valtext').html(val);
-      });
+  /* --- Val --- */
+  var val = $('input').val();
+  $('#val').click(function() {
+    $('#valtext').html(val);
+  });
 
-      /* --- Val --- */
-      $('#text').click(function() {
-        $('p#textme').text('Thank you.');
-      });
+  /* --- Val --- */
+  $('#text').click(function() {
+    $('p#textme').text('Thank you.');
+  });
 
-      /* --- Data --- */
-      var obj = {
-        one: 'Coding',
-        two: 'Dojo'
-      }
-      $('p').data('data', {
-          one: 'Coding',
-          two: 'Dojo'
-        }
-        $('#data').click(function() {
-            $('#datacontainer').text(
-            });
+  /* --- Data --- */
+  $('#data').click(function() {
+    $('#datacontainer').html($('p#datacontainer').data('role'));
+  });
 
-        });
+});
