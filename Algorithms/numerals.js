6b764246047a1330110b726ae8d4b81133f16c1f
jonathan <jgcarlson@outlook.com>
2017-06-06 13:09:24 -0700

    minor clean up

---
+++ Algorithms/numerals.js
@@ -0,0 +1,8 @@
+/*
+### Given a positive integer less than 4000, return a string containing
+### that value in Roman numeral representation. In this representation,
+### I is 1, V is 5, X is 10, L is 50, C is 100, D is 500, and M is 1000.
+### Remember that 4 is IV, 349 is CCCXLIX, and 444 is CDXLIV.
+
+### Complete in less than 45 lines. Use less than 13 conditionals
+*/
