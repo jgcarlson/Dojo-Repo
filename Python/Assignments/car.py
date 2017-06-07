956a4a60cd49dbb4c81b2a83db0e1a60937b512b
jonathan <jgcarlson@outlook.com>
2017-06-06 17:58:54 -0700

    completed car project and started product project

--- Python/Assignments/car.py
+++ Python/Assignments/car.py
@@ -14,4 +14,9 @@ class Car(object):
         print "Price: {}, Speed: {}, Fuel: {}, Mileage: {}, Tax: {}".format(str(self.price), self.speed, self.fuel, str(self.mileage), self.tax)
 
 
-red = Car(20000, '20mph', 'full', 140000)
+red = Car(20000, '80mph', 'full', 140000)
+blue = Car(2000, '80mph', 'half full', 10000)
+green = Car(1000, '99mph', 'mostly full', 126000)
+blue = Car(90000, '120mph', 'full', 567)
+blue = Car(200, '0mph', 'empty', 225000)
+blue = Car(12000, '9mph', 'almost empty', 40000)
