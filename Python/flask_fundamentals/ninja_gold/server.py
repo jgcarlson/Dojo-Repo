be48c0a8bd14f0f306ad8572cac6e2490ed182e3
jonathan <jgcarlson@outlook.com>
2017-06-09 15:49:25 -0700

    completed flask ninja gold project

--- Python/flask_fundamentals/ninja_gold/server.py
+++ Python/flask_fundamentals/ninja_gold/server.py
@@ -1,24 +1,44 @@
 from flask import Flask, render_template, request, redirect, session
 import random
 app = Flask(__name__)
-app.secret_key = 'KEEPITSECRET.KEEPITSAFE.'
+app.secret_key = 'KEEPiTSECRET.KEEPITSAFE.'
 
 
 @app.route('/')
 def index():
     if 'total' in session:
-        print 'session has total', session['total']
+        total = session['total']
+        activity = session['activities']
     else:
         session['total'] = 0
-        print 'session starting with total of ', session['total']
-    return render_template('index.html')
+        total = session['total']
+        session['activities'] = []
+        activity = session['activities']
+    return render_template('index.html', total=total, activities=activity)
 
 
 @app.route('/process_money', methods=['POST'])
 def process():
-    if request.form['farm']:
-        session['total'] += random.randint(10, 21)
-        print session['total']
+    if request.form['action'] == 'farm':
+        number = random.randint(10, 21)
+        session['total'] += number
+        session['activities'].insert(0, '<p>Went to a farm. Got ' + str(
+            number) + ' gold.</p>')
+    if request.form['action'] == 'cave':
+        number = random.randint(5, 11)
+        session['total'] += number
+        session['activities'].insert(0, '<p>Went to a cave. Got ' + str(
+            number) + ' gold.</p>')
+    if request.form['action'] == 'house':
+        number = random.randint(2, 6)
+        session['total'] += number
+        session['activities'].insert(0, '<p>Went to a house. Got ' + str(
+            number) + ' gold.</p>')
+    if request.form['action'] == 'casino':
+        number = random.randint(-51, 51)
+        session['total'] += number
+        session['activities'].insert(0, '<p>Went to a casino. Got ' + str(
+            number) + ' gold.</p>')
     return redirect('/')
 
 
