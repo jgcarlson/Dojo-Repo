9bb454db4b51f71f0e1aec2f9a6c48aaca537eb9
jonathan <jgcarlson@outlook.com>
2017-06-09 20:28:51 -0700

    nearly finished with dojo survey with validation minus results page error

--- Python/flask_fundamentals/dojo_survey3/server.py
+++ Python/flask_fundamentals/dojo_survey3/server.py
@@ -8,4 +8,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/process', methods=['POST'])
+def process():
+    if len(request.form['comments']) < 1:
+        flash('You need to tell me something.')
+        return redirect('/')
+    elif len(request.form['comments']) > 120:
+        flash('You talk too much.')
+        return redirect('/')
+    else:
+        return redirect('/submitted')
+
+
+@app.route('/submitted', methods=['POST'])
+def submitted():
+    first = request.form['first']
+    last = request.form['last']
+    language = request.form['language']
+    like = request.form['like']
+    comments = request.form['comments']
+    return render_template('submitted.html', first=first, last=last, language=language, like=like, comments=comments)
+
+
 app.run(debug=True)
