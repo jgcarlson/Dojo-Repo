ba1c1997bd627465f9f5add3e2878abed57330ad
jonathan <jgcarlson@outlook.com>
2017-06-13 14:50:00 -0700

    working on sql queries

--- Python/flask_fundamentals/test_registration/server.py
+++ Python/flask_fundamentals/test_registration/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash
+import re
 app = Flask(__name__)
 app.secret_key = 'KEEPITSECRT.KEEPITSAFE.'
 
@@ -14,9 +15,25 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    session['first'] = request.form['first']
-    session['last'] = request.form['last']
-    session['email'] = request.form['email']
+    ready = True
+    if request.form['first'].isalpha():
+        session['first'] = request.form['first']
+    else:
+        flash('Please enter name in correct format.')
+        ready = False
+    if request.form['last'].isalpha():
+        session['last'] = request.form['last']
+    else:
+        flash('Please enter name in correct format.')
+        ready = False
+    if len(request.form['email']) < 1:
+        flash('Please enter an email address.')
+        ready = False
+    elif not re.match(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', request.form['email']):
+        flash('Please enter email address in correct format.')
+        ready = False
+    else:
+        session['email'] = request.form['email']
     session['dob'] = request.form['dob']
     session['time'] = request.form['time']
     session['password'] = request.form['password']
