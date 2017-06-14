e13929e2473ca605d0217c9b6ca80bfc22335768
jonathan <jgcarlson@outlook.com>
2017-06-14 12:22:28 -0700

    more work on friends flask/sql project -- main page completed

--- Python/flask_MySQL/full_friends/server.py
+++ Python/flask_MySQL/full_friends/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 # import the Connector function
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 # connect and store the connection in "mysql" note that you pass the database name to the function
-mysql = MySQLConnector(app, 'mydb')
+mysql = MySQLConnector(app, 'friends')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
 # an example of running a query
-print mysql.query_db("SELECT * FROM users")
+print mysql.query_db("SELECT * FROM friends")
 app.run(debug=True)
