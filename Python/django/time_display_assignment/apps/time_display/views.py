d67ad9c47aa1043873b845f7081dd266d4bf3d1f
jonathan <jgcarlson@outlook.com>
2017-06-16 13:38:51 -0700

    completed time display assignment

--- Python/django/time_display_assignment/apps/time_display/views.py
+++ Python/django/time_display_assignment/apps/time_display/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import datetime
 
 # Create your views here.
+
+
+def index(request):
+    d = datetime.datetime.now()
+    request.session['date'] = d.strftime('%b %d, %Y')
+    request.session['time'] = d.strftime('%I:%M %p')
+    return render(request, 'time_display/index.html')
--- Python/django/time_display_assignment/time_display_assignment/settings.py
+++ Python/django/time_display_assignment/time_display_assignment/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.time_display',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
