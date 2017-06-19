8d3adbbfdf3e9f3f86f948ca13f13055b6ec2fcd
jonathan <jgcarlson@outlook.com>
2017-06-18 18:47:18 -0700

    restarted django nltk project because of possible naming issues

--- Python/django/nltk_project/apps/nltk_app/apps.py
+++ Python/django/nltk_project/apps/nltk_app/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class NlAppConfig(AppConfig):
-    name = 'nl_app'
+class NltkAppConfig(AppConfig):
+    name = 'nltk_app'
--- Python/django/nltk_project/nltk_project/wsgi.py
+++ Python/django/nltk_project/nltk_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nltk project.
+WSGI config for nltk_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nltk.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nltk_project.settings")
 
 application = get_wsgi_application()
