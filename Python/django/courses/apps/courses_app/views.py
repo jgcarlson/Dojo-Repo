efc93d8f566932d4db7ccbdc896e21308474e1ee
jonathan <jgcarlson@outlook.com>
2017-06-21 13:59:32 -0700

    completes courses project

--- Python/django/courses/apps/courses_app/views.py
+++ Python/django/courses/apps/courses_app/views.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Course
 
 # Create your views here.
+
+
+def index(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request, 'courses_app/index.html', context)
+
+
+def process(request):
+    Course.objects.create(
+        name=request.POST['name'], description=request.POST['description'])
+    return redirect('/')
+
+
+def delete(request):
+    Course.objects.get(id=request.POST['delete']).delete()
+    return redirect('/')
