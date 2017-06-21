5327d3d8f59e9bdf4b133c710b81a427225ec597
jonathan <jgcarlson@outlook.com>
2017-06-20 21:20:01 -0700

    completed stores and products assignment

--- Python/django/stores_and_products/apps/stores_app/models.py
+++ Python/django/stores_and_products/apps/stores_app/models.py
@@ -6,4 +6,14 @@ from django.db import models
 # Create your models here.
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Store(models.Model):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
