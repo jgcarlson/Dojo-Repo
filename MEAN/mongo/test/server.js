063d658f90387d760e7186d3d96d3b0ca6b13b86
jonathan <jgcarlson@outlook.com>
2017-07-09 21:05:16 -0700

    created mongodb integrated template, started mongo projects

---
+++ MEAN/mongo/test/server.js
@@ -0,0 +1,79 @@
+// Require the Express Module
+var express = require('express');
+// Create an Express App
+var app = express();
+// Require Mongoose
+var mongoose = require('mongoose');
+// Connect Mongoose to MongoDB
+mongoose.connect('mongodb://localhost/my_first_db'); // localhost/ + database_name
+// Use native promises
+mongoose.Promise = global.Promise;
+// Create Schema
+var UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+}, { timestamps: {} })
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+// Require Session
+var session = require('express-session');
+app.use(session({
+  secret: 'codingdojorocks', // string for encryption
+  resave: true,
+  saveUninitialized: true
+}));
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting our Views Folder Directory
+app.set('views', path.join(__dirname, './views'));
+// Setting our View Engine set to EJS
+app.set('view engine', 'ejs');
+// Require socket.io
+var io = require('socket.io').listen(server);
+// Whenever a connection event happens (the connection event is built in) run the following code
+io.sockets.on('connection', function(socket) {
+  console.log("WE ARE USING SOCKETS!");
+  console.log(socket.id);
+  //all the socket code goes in here!
+  socket.on("button_clicked", function(data) {
+    console.log('Someone clicked a button!  Reason: ' + data.reason);
+    socket.emit('server_response', {response: "sockets are the best!"});
+  });
+});
+// Setting our Server to Listen on Port: 8000
+var server = app.listen(5000, function() {
+    console.log("listening on port 5000");
+});
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    // This is the method that finds all of the users from the database
+    // Notice how the first parameter is the options for what to find and the second is the
+    //   callback function that has an error (if any) and all of the users
+    // Keep in mind that everything you want to do AFTER you get the users from the database must
+    //   happen inside of this callback for it to be synchronous
+    // Make sure you handle the case when there is an error, as well as the case when there is no error
+  })
+  res.render('index');
+})
+app.post('/users', function(req, res) {
+  console.log("POST DATA", req.body);
+  // create a new User with the name and age corresponding to those from req.body
+  var user = new User({name: req.body.name, age: req.body.age});
+  // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+  user.save(function(err) {
+    if(err) { // if there is an error console.log that something went wrong!
+      console.log('something went wrong');
+    } else { // else console.log that we did well and then redirect to the root route
+      console.log('successfully added a user!');
+      res.redirect('/');
+    }
+  });
+});
--- MEAN/node/template/server.js
+++ MEAN/node/template/server.js
@@ -1,53 +1,48 @@
-// require express
-var express = require("express");
-// path module -- try to figure out where and why we use this
-var path = require("path");
-// create the express app
+// Require the Express Module
+var express = require('express');
+// Create an Express App
 var app = express();
+// Require Mongoose
+var mongoose = require('mongoose');
+// Connect Mongoose to MongoDB
+mongoose.connect('mongodb://localhost/basic_mongoose'); // localhost/ + database_name
+// Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+// Require Session
 var session = require('express-session');
-var app = express();
-// tell the express app to listen on port 8000
-var server = app.listen(8000, function() {
-  console.log("listening on port 8000");
-});
+app.use(session({
+  secret: 'codingdojorocks', // string for encryption
+  resave: true,
+  saveUninitialized: true
+}));
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting our Views Folder Directory
+app.set('views', path.join(__dirname, './views'));
+// Setting our View Engine set to EJS
+app.set('view engine', 'ejs');
+// Require socket.io
 var io = require('socket.io').listen(server);
 // Whenever a connection event happens (the connection event is built in) run the following code
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
   console.log("WE ARE USING SOCKETS!");
   console.log(socket.id);
   //all the socket code goes in here!
   socket.on("button_clicked", function (data){
     console.log('Someone clicked a button!  Reason: ' + data.reason);
     socket.emit('server_response', {response: "sockets are the best!"});
-})
-
-})
-app.use(bodyParser.urlencoded({ extended: true }));
-// sessions
-app.use(session({
-  secret: 'codingdojorocks', // string for encryption
-  resave: true,
-  saveUninitialized: true
-}));
-// static content
-app.use(express.static(path.join(__dirname, "./static")));
-// setting up ejs and our views folder
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-// root route to render the index.ejs view
-app.get('/', function(req, res) {
-  console.log(req.body);
-  res.render('index');
+  });
 });
-// app.post('/results', function(req, res) {
-  // let data = {
-  //   name: req.body.name,
-  //   location: req.body.location,
-  //   language: req.body.language,
-  //   comment: req.body.comment
-  // }
-// });
-app.get('/results', function(req, res) {
-  res.render('results');
+// Setting our Server to Listen on Port: 8000
+var server = app.listen(5000, function() {
+    console.log("listening on port 5000");
+});
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+    res.render('index');
 });
