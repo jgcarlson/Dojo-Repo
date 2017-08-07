c613504859c5d9e3bcc3ff0e64e0f1cea959fab6
jonathan <jgcarlson@outlook.com>
2017-08-07 11:00:53 -0700

    started first java spring boot assignment

---
+++ Java/spring/string-assignment/src/main/java/com/web/stringassignment/StringAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.web.stringassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAssignmentApplication.class, args);
+	}
+}
